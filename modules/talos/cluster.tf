e5205f12e42fecbc3b94d98bc2f98a4d9fa4e772
Christopher Barnes <christopher@barnes.biz>
2025-10-30 17:25:50 +0100

    add talso resources

---
+++ modules/talos/cluster.tf
@@ -0,0 +1,5 @@
+resource "talos_cluster_kubeconfig" "this" {
+  depends_on           = [talos_machine_bootstrap.this]
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = [for k, v in var.node_data.controlplanes : k][0]
+}
---
+++ modules/talos/machine.tf
@@ -0,0 +1,29 @@
+resource "talos_machine_bootstrap" "this" {
+  depends_on = [talos_machine_configuration_apply.controlplane]
+
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = [for k, v in var.node_data.controlplanes : k][0]
+}
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+  for_each                    = var.node_data.controlplanes
+  node                        = each.key
+  config_patches = [
+    templatefile("${path.module}/templates/install-disk-and-hostname.yaml.tmpl", {
+      hostname     = each.value.hostname == null ? format("%s-cp-%s", var.cluster_name, index(keys(var.node_data.controlplanes), each.key)) : each.value.hostname
+      install_disk = each.value.install_disk
+    }),
+    file("${path.module}/files/cp-scheduling.yaml"),
+  ]
+}
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = var.cluster_endpoint
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+resource "talos_machine_secrets" "this" {}
