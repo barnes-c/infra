40a2fb422269007df97e860773d47c81775a043d
Christopher Barnes <68067715+barnes-c@users.noreply.github.com>
2026-01-31 18:52:23 +0100

    feat: enable renovate to manage helm resources (#36)

--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -10,12 +10,6 @@ variable "argocd_values" {
   default     = {}
 }
 
-variable "argocd_version" {
-  description = "ArgoCD Helm chart version for initial bootstrap"
-  type        = string
-  default     = "9.3.7"
-}
-
 variable "cilium_enabled" {
   description = "Enable Cilium CNI bootstrap"
   type        = bool
@@ -26,12 +20,6 @@ variable "cilium_values" {
   description = "Additional Helm values for Cilium (merged with defaults)"
   type        = any
   default     = {}
-}
-
-variable "cilium_version" {
-  description = "Cilium Helm chart version for initial bootstrap"
-  type        = string
-  default     = "1.18.6"
 }
 
 variable "kubeconfig" {
