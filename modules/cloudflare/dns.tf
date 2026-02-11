e64fecfe35d4d3e0165c3cc045c9e91ce4736a10
Christopher Barnes <christopher@barnes.biz>
2026-02-11 23:39:55 +0100

    feat: configure dns record for cloudflare tunnel

--- modules/cloudflare/dns.tf
+++ modules/cloudflare/dns.tf
@@ -2,10 +2,11 @@ resource "cloudflare_dns_record" "root_web_a" {
   for_each = data.cloudflare_zones.zones
 
   zone_id = each.value.result[0].id
-  content = "192.168.1.201"
+  content = "fddf397a-eb1f-4a34-957f-eb9cffeecd2d.cfargotunnel.com"
   name    = each.value.name
   type    = "A"
   ttl     = 1
+  proxied = true
 }
 
 resource "cloudflare_dns_record" "wildcard_web_a" {
